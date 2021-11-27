3f3ad89d71087bc58eafeee42ab25563b3237197
Alessandro Chumpitaz <par_214@hotmail.com>
2021-11-27 03:06:15 -0500

    Added Tutor Registered

--- src/main/java/com/arkisoftware/tutors/contracts/events/TutorRegistered.java
+++ src/main/java/com/arkisoftware/tutors/contracts/events/TutorRegistered.java
@@ -1,4 +1,14 @@
 package com.arkisoftware.tutors.contracts.events;
 
+import lombok.Value;
+
+import java.time.Instant;
+
+@Value
 public class TutorRegistered {
+    String tutorId;
+    String firstName;
+    String lastName;
+    String dni;
+    Instant ocurredOn;
 }
